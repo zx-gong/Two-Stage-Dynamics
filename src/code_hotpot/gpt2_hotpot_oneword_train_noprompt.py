fef03d262b11adebab321196ddd8b38c5af572b9
zx-gong <161433269+zx-gong@users.noreply.github.com>
2025-02-28 10:26:48 +0800

    Update gpt2_hotpot_oneword_train_noprompt.py

--- src/code_hotpot/gpt2_hotpot_oneword_train_noprompt.py
+++ src/code_hotpot/gpt2_hotpot_oneword_train_noprompt.py
@@ -246,10 +246,10 @@ if __name__ == '__main__':
     parser.add_argument('--lnum', type=int, default=24, help='Layers to edit', choices=list(range(-1, 28)))
     parser.add_argument('--model_path',
                         type=str,
-                        default="/home/zxgong/laser-main/data/Llama2/Llama-2-7b-hf",
+                        default="/data/Llama2/Llama-2-7b-hf",
                         help="Place where model weights are stored")
     parser.add_argument('--home_dir', type=str,
-                        default="/home/zxgong/laser-main/data/iclr/hotpot_oneword_noprompt/gpt2_results",
+                        default="/data/original/hotpot_oneword_noprompt/gpt2_results",
                         help='Directory where the data is')
     parser.add_argument('--dataset_file', type=str,
                         default="None",
